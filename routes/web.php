4b964beb0d140d0ec37907f8a1d1bf03de77c918
陳泓維 <tim5556618@gmail.com>
2017-10-27 00:47:20 +0800

    建立controller

--- routes/web.php
+++ routes/web.php
@@ -15,30 +15,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('student/{student_no}',function($student_no){
-    return "學號:".$student_no;
-})->where(['student_no'=>'s[0-9]{10}']);
+//Route::get('student/{student_no}',function($student_no){
+//    return "學號:".$student_no;
+//})->where(['student_no'=>'s[0-9]{10}']);
 
-Route::get('student/{student_no}/score',function($student_no){
-    return "學號:".$student_no."的所有成績";
-});
+//Route::get('student/{student_no}/score',function($student_no){
+//    return "學號:".$student_no."的所有成績";
+//});
 
-Route::get('student/{student_no}/score/{subject}',function($student_no,$subject){
-    return "學號:".$student_no."的".$subject."成績";
-});
+//Route::get('student/{student_no}/score/{subject}',function($student_no,$subject){
+//    return "學號:".$student_no."的".$subject."成績";
+//});
 
-Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
-    return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
-})->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
+//Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
+//    return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+//})->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
 
-Route::pattern('student_no','s[0-9]{10}');
-Route::get('student/{student_no}',function($student_no){
-    return "學號:".$student_no;
-});
+//Route::pattern('student_no','s[0-9]{10}');
+//Route::get('student/{student_no}',function($student_no){
+//    return "學號:".$student_no;
+//});
 
-Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
-    return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
-})->where(['subject'=>'(chinese|english|math)']);
+//Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
+//    return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+//})->where(['subject'=>'(chinese|english|math)']);//
 
 
 Route::pattern('student_no','s[0-9]{10}');
