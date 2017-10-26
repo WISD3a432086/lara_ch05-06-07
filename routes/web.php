10e2134c9d61624729f9fe37bb484377cad07f0a
陳泓維 <tim5556618@gmail.com>
2017-10-27 00:13:29 +0800

    基礎路由參數

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('student/{student_no}',function($student_no){
+    return "學號:".$student_no;
+});
+
+Route::get('student/{student_no}/score',function($student_no){
+    return "學號:".$student_no."的所有成績";
+});
+
+Route::get('student/{student_no}/score/{subject}',function($student_no,$subject){
+    return "學號:".$student_no."的".$subject."成績";
+});
