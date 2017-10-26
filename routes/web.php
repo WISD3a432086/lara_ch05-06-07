3f0405777b3a09c1f738e3e4fede4267b34b9f98
陳泓維 <tim5556618@gmail.com>
2017-10-27 00:58:22 +0800

    綁定路由和controller

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','HomeController@index');
+//function () {
+//   return view('welcome');
+//});
 
 //Route::get('student/{student_no}',function($student_no){
 //    return "學號:".$student_no;
@@ -47,14 +48,11 @@ Route::group(['prefix'=>'student'],function(){
 
     Route::get('{student_no}',[
         'as'=>'student',
-        'uses'=>function($student_no){
-            return "學號:".$student_no;
-        }
+        'uses'=>'StudentController@getStudentData'
     ]);
 
     Route::get('{student_no}/score/{subject?}',[
         'as'=>'student.score',
-        'uses'=> function($student_no,$subject=null){
-        return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
-    }])->where(['subject'=>'(chinese|english|math)']);
+        'uses'=>'StudentController@getStudentScore'
+    ])->where(['subject'=>'(chinese|english|math)']);
 });
