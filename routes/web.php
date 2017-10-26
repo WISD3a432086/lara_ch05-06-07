3ae9cf49fcc4a7696efbd1cbb53322056adef27f
陳泓維 <tim5556618@gmail.com>
2017-10-27 00:27:57 +0800

    正規表達式限制參數

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Route::get('student/{student_no}',function($student_no){
     return "學號:".$student_no;
-});
+})->where(['student_no'=>'s[0-9]{10}']);
 
 Route::get('student/{student_no}/score',function($student_no){
     return "學號:".$student_no."的所有成績";
@@ -29,5 +29,16 @@ Route::get('student/{student_no}/score/{subject}',function($student_no,$subject)
 
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+})->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
+
+Route::pattern('student_no','s[0-9]{10}');
+Route::get('student/{student_no}',function($student_no){
+    return "學號:".$student_no;
 });
+
+Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
+    return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+})->where(['subject'=>'(chinese|english|math)']);
+
+
 
