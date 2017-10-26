8756194d0796e9c9adab62b1f58ecb671f8f3571
陳泓維 <tim5556618@gmail.com>
2017-10-27 00:35:49 +0800

    路由群組

--- routes/web.php
+++ routes/web.php
@@ -41,4 +41,12 @@ Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject
 })->where(['subject'=>'(chinese|english|math)']);
 
 
-
+Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix'=>'student'],function(){
+    Route::get('{student_no}',function($student_no){
+    return "學號:".$student_no;
+});
+    Route::get('{student_no}/score/{subject?}',function($student_no,$subject=null){
+        return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+    })->where(['subject'=>'(chinese|english|math)']);
+});
