ea31a8f3ed6ea69b5543e29b71376b3e76281d8c
陳泓維 <tim5556618@gmail.com>
2017-11-15 00:42:11 +0800

    定義子示圖

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>詳細資料</title>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}" />
-	<script type="text/javascript" src="{{ asset('js/jquery.min.js') }} "></script>
-	<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
-</head>
-<body>
-	<nav class="navbar navbar-default navbar-fixed-top">
-		<div class="container">
-			<a class="navbar-brand" href="/">HelloLaravel</a>
-			<ul class="nav navbar-nav">
-				<li>
-					<a href="#">排行榜</a>
-				</li>
-			</ul>
-		</div>
-	</nav>
-	<div style="padding-top: 70px;"></div>
-	<div class="container">
+@extends('layouts.master')
+
+@section('title','排行榜')
+
+@section('content')
 		<div class="page-header">
 			<h1>排名</h1>
 		</div>
@@ -53,6 +36,4 @@
 				</tbody>
 			</table>
 		</div>
-	</div>
-</body>
-</html>
+@stop
