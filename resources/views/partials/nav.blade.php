0c1748e0f312c667c3dc8f7496b1d7b9293d52ff
陳泓維 <tim5556618@gmail.com>
2017-11-15 01:07:31 +0800

    修改nav.blade.php視圖上的超連接

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,7 +3,7 @@
         <a class="navbar-brand" href="/">HelloLaravel</a>
         <ul class="nav navbar-nav">
             <li>
-                <a href="#">排行榜</a>
+                <a href="/board">排行榜</a>
             </li>
         </ul>
     </div>
