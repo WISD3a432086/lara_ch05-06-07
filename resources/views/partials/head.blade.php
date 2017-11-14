77ecfd1bdbe01dc4107997a86cbc449338551183
陳泓維 <tim5556618@gmail.com>
2017-11-15 00:30:03 +0800

    定義共用區塊

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,4 @@
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}" />
+<script type="text/javascript" src="{{ asset('js/jquery.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
